7835301957f71fb081b2a6c78a495496deb9e793
colkito <code@colkito.xyz>
2022-01-29 12:39:28 -0300

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,16 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Check the URL starts with 'http' protocol, and redirect to 'https' if needed
-  if (typeof window !== 'undefined') {
-    const httpTokens: string[] | null = /^http:\/\/(.*)$/.exec(
-      window.location.href
-    )
-    const isDev = window.location.hostname === 'localhost'
-    if (!!httpTokens && !isDev) {
-      window.location.replace('https://' + httpTokens[1])
-      return <></>
-    }
-  }
+  // if (typeof window !== 'undefined') {
+  //   const httpTokens: string[] | null = /^http:\/\/(.*)$/.exec(
+  //     window.location.href
+  //   )
+  //   const isDev = window.location.hostname === 'localhost'
+  //   if (!!httpTokens && !isDev) {
+  //     window.location.replace('https://' + httpTokens[1])
+  //     return <></>
+  //   }
+  // }
 
   return <Component {...pageProps} />
 }
