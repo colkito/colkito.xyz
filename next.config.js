ed0a150b2de8b98f40b737a4ca395f1e6f1000ff
colkito <98502+colkito@users.noreply.github.com>
2022-06-04 20:38:56 -0300

    remove next/mdx to use remark

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
-
-module.exports = withMDX({
+module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-
-  // Support MDX files as pages:
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-})
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+}
