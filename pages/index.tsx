e3086fb314064606b16b633bf2669ee06646dad5
colkito <github@colkito.com>
2023-01-28 12:59:52 -0300

    update base code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { FaGithub } from 'react-icons/fa';
 export default function Home() {
   return (
     <>
-      hi there
       <h1 className="text-3xl font-bold underline">
         Hello world! <FaGithub />
       </h1>
